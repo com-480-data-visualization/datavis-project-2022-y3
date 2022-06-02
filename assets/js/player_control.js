cb42c25a92cd72979c3c3ad18b3d811485e3af13
Alvorecer721 <1535051507@qq.com>
2022-06-03 00:07:00 +0200

    connect player card with radar chart, delete required

--- assets/js/player_control.js
+++ assets/js/player_control.js
@@ -1,3 +1,5 @@
+import { RadarPlot } from './radar.js'
+
 function whenDocumentLoaded(action) {
     if (document.readyState === "loading") {
         document.addEventListener("DOMContentLoaded", action);
@@ -7,85 +9,109 @@ function whenDocumentLoaded(action) {
     }
 }
 
-function appendPlayerCard(selected_player){
+class playerBoard {
+    constructor() {
+        this.radar = new RadarPlot()
+        this.radar.createRadarBackground()
+        this.chosen = []
+        }
 
-    d3.csv('data/players.csv', function (error, data){
+    appendPlayerCard(selected_player){
 
-        selected_player = ['158023', '20801']
+        let radar = this.radar
+        let chosen = this.chosen
 
-        let stats = _(data)
-            .keyBy('sofifa_id')
-            .at(selected_player)
-            .value()
+        d3.csv('data/players.csv', function (error, data){
+
+            selected_player = ['158023', '20801']
+
+            let stats = _(data)
+                .keyBy('sofifa_id')
+                .at(selected_player)
+                .value()
 
 
-        stats.forEach(function (stat) {
-            let sofifa_id = stat['sofifa_id'],
-                name      = stat['short_name'],
-                overall   = stat['overall'],
-                face_img  = stat['player_face_url'],
-                position  = stat['player_positions'].split(",")[0],
-                value     = stat['value_eur'],
-                nation    = stat['nationality_name'],
-                club      = stat['club_name']
+            stats.forEach(function (stat) {
+                let sofifa_id = stat['sofifa_id'],
+                    name      = stat['short_name'],
+                    overall   = stat['overall'],
+                    face_img  = stat['player_face_url'],
+                    position  = stat['player_positions'].split(",")[0],
+                    value     = stat['value_eur'],
+                    nation    = stat['nationality_name'],
+                    club      = stat['club_name']
 
-            const player = d3.select('.swiper-wrapper')
-                .append('article')
-                .classed('player__card', true)
-                .classed('swiper-slide', true)
-                .classed('swiper-slide-duplicate', true)
-                .classed('swiper-slide-duplicate-active', true)
-                .style('background_color', 'hsl(219, 4%, 7%)')
-                .attr('id', sofifa_id)
+                const player = d3.select('.swiper-wrapper')
+                    .append('article')
+                    .classed('player__card', true)
+                    .classed('swiper-slide', true)
+                    .classed('swiper-slide-duplicate', true)
+                    .classed('swiper-slide-duplicate-active', true)
+                    .style('background_color', 'hsl(219, 4%, 7%)')
+                    .attr('id', sofifa_id)
 
-            player.append('div')
-                .classed('shape shape__smaller', true)
+                player.append('div')
+                    .classed('shape shape__smaller', true)
 
-            player.append('h1')
-                .classed('player__title', true)
-                .text(name)
+                player.append('h1')
+                    .classed('player__title', true)
+                    .text(name)
 
-            player.append('h3')
-                .classed('player__subtitle',true)
-                .text(nation)
+                player.append('h3')
+                    .classed('player__subtitle',true)
+                    .text(nation)
 
-            player.append('img')
-                .classed('player__img',true)
-                .attr("src", face_img)
+                player.append('img')
+                    .classed('player__img',true)
+                    .attr("src", face_img)
 
-            const player_data = player.append('div')
-                .classed('player__data', true)
+                const player_data = player.append('div')
+                    .classed('player__data', true)
 
-            player_data.append('div')
-                .classed('player__data-group', true)
-                .append('i')
-                .classed('ri-dashboard-3-line', true)
-                .text(overall)
+                player_data.append('div')
+                    .classed('player__data-group', true)
+                    .append('i')
+                    .classed('ri-dashboard-3-line', true)
+                    .text(overall)
 
-            player_data.append('div')
-                .classed('player__data-group', true)
-                .append('i')
-                .classed('ri-dashboard-3-line', true)
-                .text(position)
+                player_data.append('div')
+                    .classed('player__data-group', true)
+                    .append('i')
+                    .classed('ri-dashboard-3-line', true)
+                    .text(position)
 
-            player_data.append('div')
-                .classed('player__data-group', true)
-                .append('i')
-                .classed('ri-dashboard-3-line', true)
-                .text(club)
+                player_data.append('div')
+                    .classed('player__data-group', true)
+                    .append('i')
+                    .classed('ri-dashboard-3-line', true)
+                    .text(club)
 
-            player.append('h3')
-                .classed('player__price',true)
-                .text('€' + value)
+                player.append('h3')
+                    .classed('player__price',true)
+                    .text('€' + value)
 
-            player.append('button')
-                .classed('button player__button',true)
-                .on('click', function (d) {
-                    console.log(player.attr('id'));
-                })
+                player.append('button')
+                    .classed('button player__button',true)
+                    .on('click', function (_) {
+                        let selected_player=player.attr('id');
+
+                        if (chosen.includes(selected_player)) {
+                            let idx = chosen.indexOf(selected_player)
+                            chosen.splice(idx , 1)
+                        } else {
+                            chosen.push(selected_player)
+                        }
+
+                        radar.drawRadarArea(chosen)
+                    })
+            })
         })
-    })
-
+    }
 }
 
-appendPlayerCard()
+whenDocumentLoaded(() => {
+    const control = new playerBoard()
+
+    control.appendPlayerCard('dsadsa')
+});
+
--- assets/js/radar.js
+++ assets/js/radar.js
@@ -18,13 +18,12 @@ function getColor(idx) {
 }
 
 
-class RadarPlot {
+export class RadarPlot {
     constructor() {
-        const playerStats = {
-            fieldNames: ['Shoot', 'Pass', 'Dribble', 'Attack', 'Skill', 'Movement', 'Power', 'Mentality'],
-        }
+        this.fieldNames = ['Shoot', 'Pass', 'Dribble', 'Attack', 'Skill', 'Movement', 'Power', 'Mentality']
+        this.fifaId = []
 
-        const radar = {
+        this.radar = {
             width: 600,
             height: 600,
             margin: {top: 20, right: 20, bottom: 20, left: 20},
@@ -43,40 +42,44 @@ class RadarPlot {
             arc: 2 * Math.PI
         }
 
-        this.svg = d3.select(".capability__container")
-            .append("svg")
-            .attr('width', radar.width)
-            .attr('height', radar.height)
-
-
-        const onePiece = radar.arc/radar.total
+        this.onePiece = this.radar.arc/this.radar.total
 
         // 计算网轴的正多边形的坐标
-        const polygons = {
+        this.polygons = {
             webs: [],
             webPoints: []
         };
 
-        for(let k=radar.level; k>0; k--) {
+        this.svg = d3.select(".capability__container")
+            .append("svg")
+            .attr('width', this.radar.width)
+            .attr('height', this.radar.height)
+    }
+
+    createRadarBackground() {
+
+        let fields = this.fieldNames
+
+        for(let k=this.radar.level; k>0; k--) {
             let webs = '',
                 webPoints = [];
-            let r = radar.radius/radar.level * k;
-            for(let i=0; i<radar.total; i++) {
-                let x = r * Math.sin(i * onePiece),
-                    y = r * Math.cos(i * onePiece);
+            let r = this.radar.radius/this.radar.level * k;
+            for(let i=0; i<this.radar.total; i++) {
+                let x = r * Math.sin(i * this.onePiece),
+                    y = r * Math.cos(i * this.onePiece);
                 webs += x + ',' + y + ' ';
                 webPoints.push({
                     x: x,
                     y: y
                 });
             }
-            polygons.webs.push(webs);
-            polygons.webPoints.push(webPoints);
+            this.polygons.webs.push(webs);
+            this.polygons.webPoints.push(webPoints);
         }
 
         this.svg.append('g')
             .selectAll('polygon')
-            .data(polygons.webs)
+            .data(this.polygons.webs)
             .enter()
             .append('polygon')
             .attr('points', function(d) {
@@ -86,33 +89,33 @@ class RadarPlot {
             .style('fill-opacity', '0.5')
             .style('stroke', 'grey')
             .style('stroke-dasharray', '10 6')
-            .attr("transform", "translate(" + radar.width/2 + ", " + radar.height/2 + ")");
+            .attr("transform", "translate(" + this.radar.width/2 + ", " + this.radar.height/2 + ")");
 
         this.svg.append('g')
             .selectAll('line')
-            .data(polygons.webPoints[0])
+            .data(this.polygons.webPoints[0])
             .enter()
             .append('line')
             .style('fill', 'red')
             .attr('x1', 0)
             .attr('y1', 0)
             .attr('x2', function(d) {
-            return d.x;
+                return d.x;
             })
             .attr('y2', function(d) {
                 return d.y;
             })
-            .attr("transform", "translate(" + radar.width/2 + ", " + radar.height/2 + ")")
+            .attr("transform", "translate(" + this.radar.width/2 + ", " + this.radar.height/2 + ")")
             .style("stroke", "grey")
             .style('stroke-dasharray', '10 6')
 
         // Add Text FieldName
         const textPoints = []
-        const textRadius = radar.radius + 20
+        const textRadius = this.radar.radius + 20
 
-        for (let i=0; i<radar.total; i++) {
-            let x = textRadius * Math.sin(i * onePiece),
-                y = textRadius * Math.cos(i * onePiece)
+        for (let i=0; i<this.radar.total; i++) {
+            let x = textRadius * Math.sin(i * this.onePiece),
+                y = textRadius * Math.cos(i * this.onePiece)
 
             textPoints.push({
                 x: x,
@@ -132,19 +135,21 @@ class RadarPlot {
                 return d.y
             })
             .text(function(d,i) {
-                return playerStats.fieldNames[i]
+                return fields[i]
             })
             .style("font-family", "'Exo', sans-serif")
             .style("font-size", "1rem")
             .style("font-medium", '500')
             .style('font-semi-bold', '600')
-            .attr("transform", "translate(" + radar.width/2 + ", " + radar.height/2 + ")")
+            .attr("transform", "translate(" + this.radar.width/2 + ", " + this.radar.height/2 + ")")
             .attr("fill", "white")
             .attr("text-anchor", "middle")
 
         // Add text indicate %
-        for (let i = 0; i < radar.level; i++) {
-            let levelFactor = radar.factor * radar.radius * ((i + 1) / radar.level)
+        let factor = this.radar.factor
+
+        for (let i = 0; i < this.radar.level; i++) {
+            let levelFactor = this.radar.factor * this.radar.radius * ((i + 1) / this.radar.level)
             let Format = d3.format('.0%');
 
             this.svg.selectAll(".levels")
@@ -152,20 +157,25 @@ class RadarPlot {
                 .enter()
                 .append('svg:text')
                 .attr('x', function (d) {
-                    return levelFactor * (1 - radar.factor * Math.sin(0))
+                    return levelFactor * (1 - factor * Math.sin(0))
                 })
                 .attr('y', function (d) {
-                    return levelFactor * (1 - radar.factor * Math.cos(0))
+                    return levelFactor * (1 - factor * Math.cos(0))
                 })
                 .style("font-family", "sans-serif")
                 .style("font-size", "10px")
                 .style("font-size", "10px")
-                .attr("transform", "translate(" + (radar.width / 2 - levelFactor + radar.toRight) + ", " + (radar.height / 2 - levelFactor) + ")")
+                .attr("transform", "translate(" + (this.radar.width / 2 - levelFactor + this.radar.toRight) + ", " + (this.radar.height / 2 - levelFactor) + ")")
                 .attr("fill", "white")
-                .text(Format((i + 1) * radar.maxValue / radar.level))
+                .text(Format((i + 1) * this.radar.maxValue / this.radar.level))
         }
+    }
 
+
+    drawRadarArea(selected_player) {
         let radarPlot = this.svg
+        let radar     = this.radar
+        let onePiece  = this.onePiece
 
         d3.csv('data/players.csv', function (err, data) {
             // Calculate coordinates of radar chart
@@ -173,7 +183,11 @@ class RadarPlot {
             const values = [];
 
             // Collect statistics of selected players.csv
-            _.filter(data, {'short_name': 'L. Messi'}).forEach( function(player) {
+            _(data)
+                .keyBy('sofifa_id')
+                .at(selected_player)
+                .value()
+                .forEach( function(player) {
                     let shooting  = +player.shooting,
                         passing   = +player.passing,
                         dribble   = +player.dribbling,
@@ -192,6 +206,8 @@ class RadarPlot {
                 })
 
             // Calculate point coordination
+
+
             for(let i=0; i<values.length; i++) {
                 let value = values[i],
                     area = '',
@@ -280,10 +296,9 @@ class RadarPlot {
             }
         })
     }
+
 }
 
 whenDocumentLoaded(() => {
-    // console.log(s)
-
-    const plot = new RadarPlot();
+    // const plot = new RadarPlot();
 });
