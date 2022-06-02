c75836975a82fa464e658762fa31cedb3d1e51b0
Yifei <55719276+MeloS7@users.noreply.github.com>
2022-06-03 01:26:58 +0200

    Update positionPlayer.js

--- assets/js/positionPlayer.js
+++ assets/js/positionPlayer.js
@@ -100,9 +100,9 @@ $(document).ready(function() {
                 }
             })
 
-        // top3_id = [list_order_by_value[0]["sofifa_id"], list_order_by_value[1]["sofifa_id"], list_order_by_value[2]["sofifa_id"]]
-        // console.log(top3_id)
-        console.log(list_order_by_value)
+        top3_id = [list_order_by_value[0]["sofifa_id"], list_order_by_value[1]["sofifa_id"], list_order_by_value[2]["sofifa_id"]]
+        console.log(top3_id)
+        // console.log(list_order_by_value)
     })
 
     $.extend({
